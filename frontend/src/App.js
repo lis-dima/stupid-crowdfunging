d01b78b7b7d6161ea168a0ac01996e798724843b
lis-dima <lisiev.dmytro@gmail.com>
2022-11-24 14:51:12 +0000

    connect to phantom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [wallAddress, setWallAddress] = useState(null);
+  const checkWalletConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("solana.isPhantom is detected");
+          const response = await solana.connect({
+            onlyIfTrusted: true,
+          });
+          console.log("Connected with pubKey: ", response.publicKey.toString());
+          setWallAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("solana obj not found!");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const connectWallet = async () => {
+    const { solana } = window;
+    if (solana) {
+      const response = await solana.connect();
+      setWallAddress(response.publicKey.toString());
+      console.log("Connected with pubKey: ", response.publicKey.toString());
+    }
+  };
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet}>Connect to wallet</button>
   );
-}
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkWalletConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  return (
+    <div className="App">{!wallAddress && renderNotConnectedContainer()}</div>
+  );
+};
 
 export default App;
